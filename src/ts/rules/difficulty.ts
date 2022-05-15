b565073fbb5642bad3d2949ba81d11c4f266a61c
Ava Mattie <ava@mattie.codes>
2022-05-15 16:38:40 -0600

    Add difficulty rules, unit tests

---
+++ src/ts/rules/difficulty.ts
@@ -0,0 +1,88 @@
+/**
+ * Class which contains helper methods that can help deal with difficulty.
+ */
+export class DifficultyLevel {
+  readonly targetNumber: number;
+
+  constructor(
+      readonly difficulty: number,
+      readonly description: string,
+      readonly guidance: string) {
+    this.targetNumber = difficulty * 3;
+  }
+
+  static get(difficulty: number): DifficultyLevel|null {
+    if (difficulty >= DifficulyLevels.length) return null;
+    return DifficulyLevels[difficulty]!;
+  }
+
+  static getByTargetNumber(
+      targetNumber: number, difficultyMod = 0): DifficultyLevel|null {
+    const difficulty = targetNumber !== 0 ? Math.floor(targetNumber / 3) : 0;
+    return DifficultyLevel.get(difficulty + difficultyMod);
+  }
+
+  static getByRoll(roll: number, difficultyMod = 0): DifficultyLevel|null {
+    const target = roll - (roll % 3);
+    return DifficultyLevel.getByTargetNumber(target, difficultyMod);
+  }
+
+}
+
+export const DifficulyLevels = [
+  new DifficultyLevel(
+    /* difficulty= */ 0,
+    "Routine",
+    "Anyone can do this basically every time",
+  ),
+  new DifficultyLevel(
+    /* difficulty= */ 1,
+    "Simple",
+    "Most people can do this most of the time",
+  ),
+  new DifficultyLevel(
+    /* difficulty= */ 2,
+    "Standard",
+    "Typical task requiring focus, but most people can usually do this",
+  ),
+  new DifficultyLevel(
+    /* difficulty= */ 3,
+    "Demanding",
+    "Requires full attention; most people have a 50/50 chance to succeed",
+  ),
+  new DifficultyLevel(
+    /* difficulty= */ 4,
+    "Difficult",
+    "Trained people have a 50/50 chance to succeed",
+  ),
+  new DifficultyLevel(
+    /* difficulty= */ 5,
+    "Challenging",
+    "Even trained people often fail",
+  ),
+  new DifficultyLevel(
+    /* difficulty= */ 6,
+    "Intimidating",
+    "Normal people almost never succeed",
+  ),
+  new DifficultyLevel(
+    /* difficulty= */ 7,
+    "Formidable",
+    "Impossible without skills or great effort",
+  ),
+  new DifficultyLevel(
+    /* difficulty= */ 8,
+    "Heroic",
+    "A task worthy of tales told for years afterwards",
+  ),
+  new DifficultyLevel(
+    /* difficulty= */ 9,
+    "Immortal",
+    "A task worthy of legends that last for lifetimes",
+  ),
+  new DifficultyLevel(
+    /* difficulty= */ 10,
+    "Impossible",
+    "A task that normal humans couldn't consider (but doesn't break the laws of physics)",
+  ),
+];
---
+++ test/rules/difficulty.spec.ts
@@ -0,0 +1,70 @@
+import {DifficultyLevel, DifficulyLevels} from "../../src/ts/rules/difficulty";
+
+const assert = require("assert");
+
+describe("DifficultyLevels", () => {
+  it("Has 11 difficulty levels (matching the source book)", () => {
+    assert.equal(DifficulyLevels.length, 11);
+  });
+});
+
+describe("DifficultyLevel", () => {
+  describe(".get()", () => {
+    it('returns a DifficultyLevel for a valid input', () => {
+      const difficultyLevel = DifficultyLevel.get(3);
+
+      assert.notEqual(difficultyLevel, null);
+      assert.equal(difficultyLevel.difficulty, 3);
+      assert.equal(difficultyLevel.targetNumber, 9);
+    });
+    it('returns null for an invalid input', () => {
+      const difficultyLevel = DifficultyLevel.get(20);
+
+      assert.equal(difficultyLevel, null);
+    });
+  });
+  describe(".getByTargetNumber()", () => {
+    it('returns a DifficultyLevel for a valid target number', () => {
+      const difficultyLevel = DifficultyLevel.getByTargetNumber(9);
+
+      assert.notEqual(difficultyLevel, null);
+      assert.equal(difficultyLevel.difficulty, 3);
+      assert.equal(difficultyLevel.targetNumber, 9);
+    });
+    it('returns null for an invalid target number', () => {
+      const difficultyLevel = DifficultyLevel.getByTargetNumber(-2);
+
+      assert.equal(difficultyLevel, null);
+    });
+    it('correctly uses the difficultyMod', () => {
+      const difficultyLevel =
+          DifficultyLevel.getByTargetNumber(9, /** difficultyMod= */ 2);
+
+      assert.notEqual(difficultyLevel, null);
+      assert.equal(difficultyLevel.difficulty, 5);
+      assert.equal(difficultyLevel.targetNumber, 15);
+    });
+  });
+  describe(".getByRoll()", () => {
+    it('returns a DifficultyLevel for a valid roll', () => {
+      const difficultyLevel = DifficultyLevel.getByRoll(11);
+
+      assert.notEqual(difficultyLevel, null);
+      assert.equal(difficultyLevel.difficulty, 3);
+      assert.equal(difficultyLevel.targetNumber, 9);
+    });
+    it('returns null for an invalid roll', () => {
+      const difficultyLevel = DifficultyLevel.getByRoll(50);
+
+      assert.equal(difficultyLevel, null);
+    });
+    it('correctly uses the difficultyMod', () => {
+      const difficultyLevel =
+          DifficultyLevel.getByRoll(11, /** difficultyMod= */ 1);
+
+      assert.notEqual(difficultyLevel, null);
+      assert.equal(difficultyLevel.difficulty, 4);
+      assert.equal(difficultyLevel.targetNumber, 12);
+    });
+  });
+});
